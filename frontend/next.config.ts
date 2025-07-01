c1bf00855b2ca3ae4e532a66922ee95b9a68d292
Prince Desai <149474959+princedesai012@users.noreply.github.com>
2025-07-01 22:35:38 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: "src/app", // 👈 tells Next where your App Router folder is
+  },
 };
 
 export default nextConfig;

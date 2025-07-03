eca534930b55b35e5167a8f8435494ba9479ff16
Prince Desai <149474959+princedesai012@users.noreply.github.com>
2025-07-03 15:53:09 +0530

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body 
+{/*         className={`${geistSans.variable} ${geistMono.variable} antialiased`} */}
+        >
         <Providers> {/* ✅ Wrap only the client context here */}
           {children}
         </Providers>
